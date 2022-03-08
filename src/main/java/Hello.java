58d588ad66884321fe8cdb35c99b03f6dc23ce29
ziyue <3152602224@qq.com>
2022-03-08 21:22:39 +0800

    123

---
+++ src/main/java/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: ziyue
+ * @Date: 2022/03/08/20:46
+ * @Description: 因为先生在天下
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
---
+++ src/main/java/Hello02.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: ziyue
+ * @Date: 2022/03/08/20:46
+ * @Description: 因为先生在天下
+ */
+public class Hello02 {
+    public static void main(String[] args) {
+        System.out.println("这个是第二个");
+    }
+}
